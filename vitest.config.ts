98ca4cf5c5a1f264a1bb04f7fa8db75f413b0aae
luiz14augusto <luiz14-augusto@hotmail.com>
2025-09-21 16:57:19 -0300

    fixing_ci

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,22 +2,11 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    environment: 'node',
-    globals: true,
     include: ['src/**/*.spec.ts'],
+    passWithNoTests: true,
     coverage: {
       provider: 'v8',
-      reporter: ['text', 'lcov', 'html'],
-      reportsDirectory: './coverage',
-      all: true,
-      include: ['src/**/*.ts'],
-      exclude: [
-        'src/main.ts',
-        '**/*.d.ts',
-        '**/dist/**',
-        '**/prisma/**',
-        '**/__mocks__/**'
-      ],
+      reporter: ['text', 'lcov', 'html']
     }
   }
 });
